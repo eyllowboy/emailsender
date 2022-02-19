5b092639b11f9dd08bd9fb20a52f9d83165f5287
eyllowboy <si-roga@yandex.ru>
2022-02-19 19:27:52 +0300

    sevent

--- src/main/java/com/sendemail/servlet/EmailServlet.java
+++ src/main/java/com/sendemail/servlet/EmailServlet.java
@@ -69,7 +69,8 @@ public class EmailServlet extends HttpServlet {
            System.out.println(body);
            String json = body.toString();
           CreatePdf pdf = new CreatePdf();
-          pdf.create("",json);
+           String basePath = request.getServletContext().getRealPath("/");
+          pdf.create(basePath,json);
 
            emailService = new EmailService();
           // List<String> em = emailService.getEmails();
